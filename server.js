535edb331fd74d8c64ca11b7a45063fcd8a4ff25
Jonathan-Mubalo <mubalokalombejonathan@gmail.com>
2026-08-05 18:09:20 +0200

    unistalled firebase

--- server.js
+++ server.js
@@ -7,167 +7,277 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-
 import cors from 'cors';
 // Allow requests specifically from your frontend port
 app.use(cors({ origin: 'http://localhost:5173' }));
 
-async function basicAuth(req, res, next) {
-  const authHeader = req.headers.authorization;
-
-  // Get the user/password from http headers
-  if (!authHeader || !authHeader.startsWith("Basic ")) {
-    return res.status(401).json({ message: "Authorization header missing or invalid" });
-  }
-
-  // Split the credentials into a user/password
-  const base64Credentials = authHeader.split(" ")[1];
-  const credentials = base64.decode(base64Credentials).split(":");
-  const email = credentials[0];
-  const password = credentials[1];
-
-  // Read MongoDB 
-  const collection = db.collection("Users");
-  const user = await collection.findOne({ email });
-
-  // If user is not found
-  if (!user) {
-    return res.status(401).json({ message: "User not found" });
-  }
-
-  // Decode and check the password
-  const decodedStoredPassword = base64.decode(user.password);
-  if (decodedStoredPassword !== password) {
-    return res.status(401).json({ message: "Invalid password" });
-  }
-  req.user = user;
-  next();
-}
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.error("Connection Error:", err);
+  });
 
 
-
-// End point to handle signup
+// SIGNUP ENDPOINT
 app.post("/signup", async (req, res) => {
+  try {
+    const { signupName, signupEmail, signupPassword } = req.body;
 
-});
+    if (!signupName || !signupEmail || !signupPassword) {
+      return res.status(401).json({ message: "Please fill in all the input fields" });
+    }
 
+    const collection = mongoose.connection.collection("users");
+    const userEmail = await collection.findOne({ email: signupEmail })
+    if (userEmail) {
+      return res.status(409).json({ message: "Email is already in use", info: userEmail })
+    }
 
+    else {
 
-// Endpoint to login 
-app.post("/login", async (req, res) => {
- 
+      const newSignupName = signupName.replaceAll(" ", "").toLowerCase();
+      const result = await collection.insertOne({
+        email: signupEmail,
+        role: "customer",
+        password: signupPassword,
+        userName: "@" + newSignupName,
+        createrAt: new Date()
+      })
+
+      return res.status(200).json({ message: "User successfully signed up.", info: result })
+    }
+
+  }
+
+  catch (error) {
+    console.error("Error signing up: ", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 })
 
 
-
-// Apply the basicAuth to all the endpoints that require authentication besides the sign up
-app.use(basicAuth);
-
-
-
-// Endpoint for displaying all the products that we have
-app.get("/products", async (req, res) => {
+// LOGIN ENDPOINT
+app.post("/login", async (req, res) => {
   try {
-    const collection = db.collection("Products");
+    const { loginEmail, loginPassword } = req.body;
 
-    const allProducts = await collection.find({}).toArray();
-
-    if (!allProducts || allProducts.length === 0) {
-      return res.status(404).json({ message: "No products found" });
+    if (!loginEmail || !loginPassword) {
+      return res.status(401).json({ messageType: "errorDisplay", message: "Please fill in all the form inputs to log in." });
     }
 
-    res.status(200).json(allProducts);
-  } catch (error) {
-    console.error("Error fetching all products", error);
-    res.status(500).json({ message: "Internal server Error" });
-  }
-});
+    const collection = mongoose.connection.collection("users");
 
+    const userEmail = await collection.findOne({ userEmail: loginEmail });
 
-
-// Endpoint for viewing extra information about the product details
-app.get("/productdetail/:productId", async (req, res) => {
-  try {
-    const collection = db.collection("Products");
-
-    const productDetail = await collection.findOne({ productId: req.params.productId });
-    if (!productDetail) {
-      return res.status(404).json({ message: 'Product not found' });
+    if (!userEmail) {
+      return res.status(401).json({ messageType: "errorMessage", message: "Account not found; Signup?" })
     }
-    res.status(200).json(productDetail);
-  } catch (error) {
-    console.error("Error fetching product's details", error);
-    res.status(500).json({ message: "Internal server Error" });
-  }
 
+    if (userEmail.password !== password) {
+      return res.status(401).json({ messageType: "errorMessage", message: "Password or email is incorrect." })
+    }
+
+    if (userEmail.password === password) {
+      return res.status(200).json({ messageType: "loginOkay", message: "Welcome back to ...'Our website name', accessToken", accessToken: { role: userEmail.role, password: userEmail.password, email: userEmail.email } });
+    }
+
+  } catch (error) {
+    console.error("Error logging in: ", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 
 
 // Endpoint for resetting a password from the users profile
-app.put("/passwordReset", async (req, res) => {
+app.put("/resetPassword", async (req, res) => {
   try {
-   
-    
+
+    const { userId, email, password, resetPassword, confirmPassword, userName } = req.body;
+
+    const collection = mongoose.connection.collection("users");
+
+    if (password === resetPassword) {
+
+      return res.status(409).json({ message: "Cannot update the current password with the same password." })
+    }
+
+    if (password && resetPassword === confirmPassword) {
+      const user = await collection.updateOne({ userId: userId }, { $set: { password: resetPassword } });
+      return res.status(200).json({ message: "Password has been updated" })
+    }
+
+
   } catch (error) {
- 
+    console.error("Error resetting the password: ", error);
+    return res.status(500).json({ message: "Internal Server Error" })
   }
 });
+
 
 
 // Endpoint to get your user profile
-app.get("/myProfile", async (req, res) => {
+app.get("/myProfile/:userId", async (req, res) => {
   try {
-  
-  } catch (error) {
 
+    const collection = await mongoose.connection.collection("users");
+    const { userId } = req.params;
+
+    const user = await collection.findOne({ userId })
+
+    return res.status(200).json({ message: "Data successfully collected", userProfileInfo: user })
+
+  } catch (error) {
+    console.error("Unable to get user profile: ", error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 });
+
+
 
 // Endpoint to get a record of all of the seats you have ever paid for ; we get it using ur unique user id
-app.get("/seatPaymentsHistory", async (req, res) => {
+app.get("/seatPaymentsHistory/:userId", async (req, res) => {
   try {
-  
-  } catch (error) {
 
+    const collection = await mongoose.connection.collection("payments");
+    const { userId } = req.params
+
+    const user = await collection.findMany({ userId }).toArray();
+
+    if (!user) {
+      return res.status(404).json({ message: "No booking history available" })
+    }
+    return res.status(200).json({ message: "Seat history successfully found", seatHistory: user })
+
+  } catch (error) {
+    console.error("Error getting Payment History: ", error);
+    return res.status(500).json({ message: "Internal server Error" });
   }
 });
+
+
 
 // End used by an admin to make a user a venue manager
 app.post("/changeUserRoles", async (req, res) => {
   try {
-    
-  } catch {
+
+    const { userName, role } = req.body;
+
+    const collection = await mongoose.connection.collection("users");
   
-}
+     const user = await collection.findOne({ userName });
+    if(!user){
+      return res.status(409).json({message: "Unable to find user"});
+    }
+
+    if (role == "manager") {
+      const user = await collection.updateOne({ userName }, { $set: { role } });
+      const change = await collection.findOne({ userName })
+      return res.status(200).json({
+        message: "User role has been successfully changed to manager", changedRole: change
+      })
+    }
+    else if (role == "customer") {
+      const user = await collection.updateOne({ userName }, { $set: { role } });
+      const change = await collection.findOne({ userName })
+      return res.status(200).json({
+        message: "User role has been successfully changed to customer", changedRole: change
+      })
+    }
+
+  } catch (error) {
+    console.error("During changing the users role: ", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
+
+// Endpoint used to get the user whose role we want to change 
+app.get("/getUserByUserName/:userName", async (req, res) => {
+  try{
+    
+    const collection = mongoose.connection.collection("users");
+
+    const { userName } = req.params; 
+
+    const user = await collection.findOne({ userName });
+
+    if(!user){
+      return res.status(404).json({message: "User not found."});
+    }
+
+    else{
+      return res.status(200).json({message:"User found.", userName: user.userName, userRole: user.role })
+    }
+  }
+  catch (error){
+   console.error("Error finding user: ",error);
+   return res.status(500).json({message:"Internal Server Error"});
+  }
+})
+
 
 // End used by a user to book a seat at the cinema
 app.post("/bookingSeat", async (req, res) => {
   try {
-    
-  } catch {
-  
+
+    const collection = await mongoose.connection.collection("payments");
+
+const { userId, bookingPrice, eventDate, bookedBy, numberOfSeats, venueName, address } = req.body;
+// Do not need to have the userid validted but need the booking id counter to create the booking id for the event
+
+if( !bookingPrice || !eventDate || !bookedBy || !venueName || !address ){
+  return res.status(409).json({ message:"Please fill in all the required fields before booking the venue." });
 }
+
+else {
+
+  const user = await collection.insertOne({
+    ...req.body, createdAt: new Date()
+  });
+
+  res.status(200).json({message:"User created successfully"})
+}
+  } catch (error){
+    console.error("booking a seat endpoint: ", error);
+    return res.status(500).json({ message:"Internal Server Error" });
+
+  }
 });
+
+
 
 // End used by a manager to create a new venue that they own
 app.post("/newVenue", async (req, res) => {
   try {
-    
-  } catch {
-  
-}
-});
 
-// Endpoint used to delete a venue that will no longer be shown on the app
-app.delete("/removeMyVenue", async (req, res) => {
-  try {
-h
-  } catch (error) {
+  } catch {
 
   }
 });
+
+
+
+// Endpoint used to delete a venue that will no longer be shown on the app
+app.delete("/removeMyVenue/venueName", async (req, res) => {
+  try {
+    const { venueName } = req.params
+  const collection = mongoose.connection.collection("venues");
+  const venue = await collection.findOne({ venueName })
+
+  if( !venue ){
+    return res.status(404).json({ message: "Venue does not exist." });
+  } else{
+    await collection.deleteOne({ venueName })
+  }
+  } catch (error) {
+console.error("Error deleting a venue: ",error);
+return res.status(500).json({message: "Internal Server Error"});
+  }
+});
+
+
 
 // Endpoint used to update the information of an existing venue
 app.put("/venueUpdate", async (req, res) => {
@@ -184,7 +294,7 @@ app.put("/venueUpdate", async (req, res) => {
 app.get("/venueBookingHistory", async (req, res) => {
   try {
 
-  
+
   } catch (error) {
 
   }
@@ -195,37 +305,42 @@ app.get("/venueBookingHistory", async (req, res) => {
 // Endpoint used to book a venue
 app.post("/bookingVenue", async (req, res) => {
   try {
-5
+    5
   } catch (error) {
- 
+
   }
 });
+
+
 
 // Edit an event that is already public
 app.put("/editUpcomingEvent", async (req, res) => {
   try {
-h
+    h
   } catch (error) {
 
   }
 });
+
+
 
 // Endpoint used to get all the events that are coming to display it
 app.get("/upcomingEvent", async (req, res) => {
   try {
-f
+    f
   } catch (error) {
 
   }
 });
 
 
+
 // Endpoint used to post review forms
-app.post("/postforms", async (req, res) => {
+app.post("/postForms", async (req, res) => {
 
   try {
     const { email, subject } = req.body;
-    const collection = db.collection("Contact");
+    const collection = mongoose.connection.collection("Contact");
 
     if (!email || !subject) {
       return res.status(404).json({ message: "Email and subject are required" });
@@ -238,14 +353,6 @@ app.post("/postforms", async (req, res) => {
     console.error("Unable to send and store the request", error);
     res.status(500).json({ message: "Intenral server error" });
   }
-});
-
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("MongoDB Connected");
-})
-.catch((err) => {
-    console.error("Connection Error:", err);
 });
 
 app.listen(PORT, () => {
